6aec7586b07b6910cc7ce2a690ce6035cac18eb7
Qinghong Fan <fanqinghong13@gmail.com>
2024-11-20 17:02:19 -0600

    firebase service

---
+++ app/services/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBA5j9IlyfRSDzNMqOkR6Jh-Cu1ezPurkA",
+  authDomain: "project2-f4147.firebaseapp.com",
+  projectId: "project2-f4147",
+  storageBucket: "project2-f4147.firebasestorage.app",
+  messagingSenderId: "1009472379533",
+  appId: "1:1009472379533:web:7d80946a40e937612e20c4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
