899a1b8604b12d991527f470a33341d62f4b4329
Qinghong Fan <90348969+Stephen-Fan@users.noreply.github.com>
2024-11-26 17:26:30 -0600

    update map

--- app/controllers/map.js
+++ app/controllers/map.js
@@ -1,9 +1,11 @@
 import Controller from '@ember/controller';
-// import { inject as service } from '@ember/service';
+import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class MapController extends Controller {
+  @service firebase;
+  @service router;
   @tracked lat = 44.9778; // Default latitude
   @tracked lng = -93.2650; // Default longitude
   @tracked zoom = 12; // Default zoom level
